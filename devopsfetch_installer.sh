c9671b1f3bb48720fffe33bea7ae0093b33dfcda
jadesolax <oluwolejadesolax@gmail.com>
2024-07-29 07:43:03 +0100

    another sh update

--- devopsfetch_installer.sh
+++ devopsfetch_installer.sh
@@ -21,14 +21,18 @@ echo \
 sudo apt-get update
 
 # Install necessary dependencies
-sudo apt-get install -y python3 python3-venv nginx containerd.io
+sudo apt-get install -y python3 python3-venv python3-pip nginx containerd.io
 
 # Create and activate virtual environment
-python3 -m venv devopsfetch
-source devopsfetch/bin/activate
+python3 -m venv venv
+source venv/bin/activate
+
 
 # Install Python packages
 pip install psutil tabulate
+
+# Deactivate the virtual environment
+deactivate
 
 # Copy devopsfetch.py to /usr/local/bin
 sudo cp devopsfetch.py /usr/local/bin/devopsfetch
@@ -36,6 +40,10 @@ sudo chmod +x /usr/local/bin/devopsfetch
 
 # Create log directory
 sudo mkdir -p /var/log/devopsfetch
+sudo chown $USER:$USER /var/log/devopsfetch
+
+# Add the current user to the Docker group for Docker access
+sudo usermod -aG docker $USER
 
 # Create systemd service file
 sudo bash -c 'cat <<EOF > /etc/systemd/system/devopsfetch.service
